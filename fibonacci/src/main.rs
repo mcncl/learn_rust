7dabddd7f4153f998124ffcd318a47d054118729
ben-mcnicholl <ben.mcnicholl@rea-group.com>
2021-07-03 16:58:55 +1000

    Completed Fibonacci nth position program

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,7 +1,24 @@
-fn main() {
-  println!("Fibonacci Numbers");
+use std::io;
 
-  println!("Enter a number to find out its positional Fibonacci value.")
+fn main() {
+  loop {
+    println!("Fibonacci Numbers");
+
+    println!("Enter a number to find out its positional Fibonacci value.");
+
+    let mut input = String::new();
+
+    io::stdin()
+      .read_line(&mut input)
+      .expect("Issue reading input");
+
+    let input: u32 = match input.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue
+    };
+
+    println!("The value of Fibonacci position {} is {}", input, fibonacci(input))
+  }
 }
 
 fn fibonacci(n: u32) -> u32 {
