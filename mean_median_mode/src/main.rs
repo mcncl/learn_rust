57b193b9499d3f43ebf3240a234a3ae1a93275d6
ben-mcnicholl <ben.mcnicholl@rea-group.com>
2021-07-06 09:49:05 +1000

    Ran rustfmt to clean up code

--- mean_median_mode/src/main.rs
+++ mean_median_mode/src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     let mut v = vec![4, 11, 3, 32, 5, 54, 3, 8, 9];
     let mut sum = 0;
 
-     // Loop through values in V, add their value to SUM
+    // Loop through values in V, add their value to SUM
     for i in &v {
-      sum += i;
+        sum += i;
     }
-    
+
     println!("Calculating mean, median & mode for {:?}\n", v);
     mean(sum as f64, v.len() as f64);
     println!("The median value is {}", median(&mut v));
@@ -16,20 +16,20 @@ fn main() {
 }
 
 fn mean(n: f64, l: f64) {
-  println!("The mean value is {}", f64::from(n/l));
+    println!("The mean value is {}", f64::from(n / l));
 }
 
 fn median(a: &mut [u32]) -> u32 {
-  a.sort();
-  let mid = a.len() / 2;
-  a[mid]
+    a.sort();
+    let mid = a.len() / 2;
+    a[mid]
 }
 
 fn mode(vec: &[u32]) -> Option<u32> {
-  let mut counts = HashMap::new();
-  vec.iter().copied().max_by_key(|&n| {
-    let count = counts.entry(n).or_insert(0);
-    *count += 1;
-    *count
-  })
+    let mut counts = HashMap::new();
+    vec.iter().copied().max_by_key(|&n| {
+        let count = counts.entry(n).or_insert(0);
+        *count += 1;
+        *count
+    })
 }
